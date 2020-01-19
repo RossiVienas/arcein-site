be888f5e57c559e5ef9ce959619e788a89c21eaa
RossiVienas <Dissmind@gmail.com>
2020-01-19 18:55:39 +0200

    [ADD] greeting

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { GreetingComponent } from './components/greeting/greeting.component';
+import { NewsComponent } from './components/news/news.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    GreetingComponent
+    GreetingComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
