95e8680edfef4ca03030c671f216436fdea5f721
RossiVienas <Dissmind@gmail.com>
2020-01-19 19:13:45 +0200

    [ADD] monitoring components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { GreetingComponent } from './components/greeting/greeting.component';
 import { NewsComponent } from './components/news/news.component';
+import { MonitoringComponent } from './components/monitoring/monitoring.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     GreetingComponent,
-    NewsComponent
+    NewsComponent,
+    MonitoringComponent
   ],
   imports: [
     BrowserModule,
