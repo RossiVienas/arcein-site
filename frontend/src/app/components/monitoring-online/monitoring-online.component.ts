18c78a285fd0075ccc47fd7cf18031d695058374
dreemlees <dreemlees@yandex.ru>
2020-01-22 02:10:08 +0300

    [FIX] Minor codestyle fixes

--- frontend/src/app/components/monitoring-online/monitoring-online.component.ts
+++ frontend/src/app/components/monitoring-online/monitoring-online.component.ts
@@ -17,10 +17,12 @@ export class MonitoringOnlineComponent implements OnInit {
   @Input() backColor : string = "rgb(255, 255, 255)";
   
   @ViewChild('canv', {static: true})
-  canvas      : ElementRef<HTMLCanvasElement>;
-  canvas2d    : CanvasRenderingContext2D;
-  currentValue: number = 0.4;
-  targetValue : number; 
+  private canvas      : ElementRef<HTMLCanvasElement>;
+  private canvas2d    : CanvasRenderingContext2D;
+  private currentValue: number = Math.PI / 2;
+
+//  TODO:
+//  targetValue : number; 
 
   constructor() 
   {
@@ -28,27 +30,38 @@ export class MonitoringOnlineComponent implements OnInit {
 
   drawValue(value: number) : void
   {
+    //Setting degree
+    value = Math.max(Math.min(1.0, value), 0);
+    this.currentValue = Math.PI * 2 * value + this.startAngle;
+
+    //Size
     this.canvas.nativeElement.width  = this.radius * 2 + this.stroke * 2;
     this.canvas.nativeElement.height = this.radius * 2 + this.stroke * 2;
+    
+    //Position
     this.canvas.nativeElement.style.top = (this.y) + "px";
     this.canvas.nativeElement.style.left = (this.x) + "px";
     console.log(this.canvas.nativeElement.style.left);
     console.log(this.canvas.nativeElement.style.top);
     this.canvas.nativeElement.style.position = "relative";
+    
+    //Clear
     this.canvas2d.clearRect(0, 0, this.radius*2, this.radius*2);
+    
+    //Set stroke
     this.canvas2d.lineWidth = this.stroke;     
     
+    //Draw background circle
     this.canvas2d.strokeStyle = this.backColor;
     this.canvas2d.beginPath();
-    this.canvas2d.arc(this.radius + this.stroke, this.radius + this.stroke, this.radius, this.startAngle, 2*Math.PI, false);
+    this.canvas2d.arc(this.radius + this.stroke, this.radius + this.stroke, this.radius, this.startAngle, 2 * Math.PI, false);
     this.canvas2d.stroke();
 
+    //Draw main circle
     this.canvas2d.strokeStyle = this.color;
     this.canvas2d.beginPath();
     this.canvas2d.arc(this.radius + this.stroke, this.radius + this.stroke, this.radius, this.startAngle, this.currentValue, false);
     this.canvas2d.stroke();
-
-    
   }
 
   ngOnInit() 
@@ -58,7 +71,7 @@ export class MonitoringOnlineComponent implements OnInit {
     {
       throw new Error('This browser does not support 2-dimensional canvas rendering contexts.');
     }
-    this.drawValue(0.2);
+    this.drawValue(1/3);
   }
 
   }
