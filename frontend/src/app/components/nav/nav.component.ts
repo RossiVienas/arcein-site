48b82f6b16200cab2624b0fb7a85f72e39efbfea
dreemlees <dreemlees@yandex.ru>
2020-01-24 01:10:39 +0300

    [FIX] Fixed bar-opacity script

--- frontend/src/app/components/nav/nav.component.ts
+++ frontend/src/app/components/nav/nav.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
-@Component({
+@Component
+({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements OnInit 
+{
 
   @ViewChild('nav', {static: true})
   private nav : ElementRef<HTMLUListElement>;
@@ -17,23 +19,13 @@ export class NavComponent implements OnInit {
   {
 
     // Handlers
-    this.navBar();
+    this.initHandlers();
 
-
-    // window.addEventListener('scroll', () =>
-    // {
-    //   if(pageYOffset < document.documentElement.clientHeight * 1.0 - 50)
-    //     if(!this.isHovered) this.nav.nativeElement.style.opacity = "0.7";
-    //   else this.nav.nativeElement.style.opacity = "1.0";
-    // });
   }
 
   // ===-- Event handlers --===
-
-  // Navigation bar
-  private navBar() {
-
-    // Mouse over - opacity change
+  private initHandlers() 
+  {
     this.nav.nativeElement.onmouseover = () =>
     {
       this.isHovered = true;
@@ -43,9 +35,22 @@ export class NavComponent implements OnInit {
     this.nav.nativeElement.onmouseleave = () =>
     {
       this.isHovered = false;
-      if(pageYOffset < document.documentElement.clientHeight * 1.0 - 50)
+      if(pageYOffset < document.documentElement.clientHeight - 50)
+      {
         this.nav.nativeElement.style.opacity = "0.7";
+      }
     }
+
+    window.addEventListener('scroll', () => 
+    {
+      
+      if(pageYOffset < document.documentElement.clientHeight - 50)
+      {
+        if(!this.isHovered) this.nav.nativeElement.style.opacity = "0.7";
+      }
+      else this.nav.nativeElement.style.opacity = "1.0";
+      
+    });
   }
 
 }
